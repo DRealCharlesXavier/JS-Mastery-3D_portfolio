c97c66d02593af2656d2ea6233d47fbe1822ad1a
drealcharlesxavier@proton.me <DRealCharlesXavier@proton.me>
2024-10-25 09:30:38 -1100

    1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,33 @@
 import { Canvas } from "@react-three/fiber";
 import React, { Suspense } from "react";
 import Loader from "../components/Loader";
+import Island from "../models/Island";
 
-{/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+{
+  /* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
   Popup
-</div> */}
+</div> */
+}
 const Home = () => {
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43]
+    let rotation = {0.1, 4.7, 0}
+
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+
+    return [screenScale, screenPosition]
+  }
+
+  const [isLandScale, islandPosition] = adjustIslandForScreenSize();
+
   return (
     <section className="w-full h-screen relative">
-      <Canvas 
+      <Canvas
         className="w-full h-screen bg-transparent"
         camera={{ near: 0.1, far: 1000 }}
       >
@@ -18,6 +37,11 @@ const Home = () => {
           <pointLight />
           <spotLight />
           <hemisphereLight />
+
+          <Island
+            position={islandPosition}
+            scale={islandScale}
+          />
         </Suspense>
       </Canvas>
     </section>
