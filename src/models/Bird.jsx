93df31f75e7540ca7fbfd1534a5fada9790a0e39
drealcharlesxavier@proton.me <DRealCharlesXavier@proton.me>
2024-10-30 04:12:20 -1100

    2

--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -2,6 +2,7 @@ import { useRef, useEffect } from 'react'
 
 import birdScene from '../assets/3d/bird.glb';
 import { useAnimations, useGLTF } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
 
 const Bird = () => {
   const birdRef = useRef();
@@ -11,6 +12,10 @@ const Bird = () => {
   useEffect(() => {
     actions["Take 001"].play();
   }, []);
+
+  useFrame((_, delta) => {
+    birdRef.current.rotation.y += 0.15 * delta
+  });
 
   return (
     <mesh
