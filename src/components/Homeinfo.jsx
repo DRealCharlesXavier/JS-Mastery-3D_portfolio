b15647e6a6b5705652172cf664c50bef0ffab926
drealcharlesxavier@proton.me <DRealCharlesXavier@proton.me>
2024-10-31 16:17:01 -1100

    6

--- src/components/Homeinfo.jsx
+++ src/components/Homeinfo.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 
 const renderContent = {
-  1: <h1>1</h1>,
+ 1: <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">
+  Hi, I am <span className="font-semibold">Xavier</span>
+  </h1>,
   2: <h1>2</h1>,
   3: <h1>3</h1>,
   4: <h1>4</h1>,
 };
 
 const InfoBox = ({ text, link, btnText }) => (
- <div>
-
+ <div className="info-box">
+  {text}
 </div>);
 
 const Homeinfo = ({ currentStage }) => {
