3636ea2808b8bbc00b53b348fa8792b23bcb8746
drealcharlesxavier@proton.me <DRealCharlesXavier@proton.me>
2024-10-31 16:05:05 -1100

    4

--- src/components/Homeinfo.jsx
+++ src/components/Homeinfo.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+
+const renderContent = {
+  1: <h1>1</h1>,
+  2: <h1>2</h1>,
+  3: <h1>3</h1>,
+  4: <h1>4</h1>,
+};
 
 const Homeinfo = () => {
-  return (
-    <div>Homeinfo</div>
-  )
-}
+  return <div>Homeinfo</div>;
+};
 
-export default Homeinfo
+export default Homeinfo;
