ea41694e7fc8e5498388b3b89a5f4c8654475a4a
drealcharlesxavier@proton.me <DRealCharlesXavier@proton.me>
2024-11-03 21:19:18 -1100

    2

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const [form, setForm] = useState({ name: "", email: "", message: "" });
 
-export default Contact
+  const handleChange = () => {};
+  const handleFocus = () => {};
+  const handleBlur = () => {};
+
+  return (
+    <section className="relative flex lg:flex-row flex-col max-container">
+      <div className="flex-1 min-w-[50%] flex flex-col">
+        <h1 className="head-text">Get in touch</h1>
+
+        <form className="w-full flex flex-col gap-7 mt-14">
+          <label className="text-black-500 font-semibold">
+            Name
+            <input
+              type="text"
+              name="name"
+              className="input"
+              placeholder="Xavier"
+              required
+              value={form.name}
+              onChange={handleChange}
+            />
+          </label>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
