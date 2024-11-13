7cd5996894e9efcd4d370f6173e96714b46e0161
drealcharlesxavier@proton.me <DRealCharlesXavier@proton.me>
2024-11-13 10:55:50 -1100

    1

--- src/models/Fox.jsx
+++ src/models/Fox.jsx
@@ -11,18 +11,18 @@ import { useGLTF, useAnimations } from "@react-three/drei";
 
 import scene from "../assets/3d/fox.glb"
 
-const Fox = ({ currentAnimation, ...props }) {
+const Fox = ({ currentAnimation, ...props }) => {
   const group = useRef();
-  const { nodes, materials, animations } = useGLTF("/fox.glb");
+  const { nodes, materials, animations } = useGLTF(scene);
   const { actions } = useAnimations(animations, group);
 
   useEffect(() => {
 
-  }, [action, currentAnimation])
+  }, [actions, currentAnimation])
 
   return (
     <group ref={group} {...props} dispose={null}>
-      <group name="Sketchfab_Scene">
+      <group name="Sketchfab_Scene"> 
         <primitive object={nodes.GLTF_created_0_rootJoint} />
         <skinnedMesh
           name="Object_7"
