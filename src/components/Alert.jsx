ab271b1a870151a92081822fca168cbf918bb8fe
drealcharlesxavier@proton.me <DRealCharlesXavier@proton.me>
2024-12-03 23:53:22 -1100

    1

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
-const Alert = () => {
+const Alert = ({ type, text }) => {
   return (
-    <div>Alert</div>
-  )
-}
+    <div className="absolute top-10 left-0 right-0 flex justify-center items-center">
+      <div
+        className={`${
+          type === "danger" ? "bg-red-500" : "bg-blue-800"
+        } p-2 text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex`}
+        role="alert"
+      >
+        <p className="mr-2">{text}</p>
+      </div>
+    </div>
+  );
+};
 
-export default Alert
+export default Alert;
