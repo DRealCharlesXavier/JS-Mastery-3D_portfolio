7d4d9cf664a09a52d8f3d344937b31577aee5db9
drealcharlesxavier@proton.me <DRealCharlesXavier@proton.me>
2024-12-04 15:03:38 -1100

    3

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { skills } from '../constants'
 
 const About = () => {
   return (
@@ -17,7 +18,18 @@ const About = () => {
         <h3 className='subhead-text'>My Skills</h3>
 
         <div className='mt-16 flex flex-wrap gap-12'>
-
+          {skills.map((skill) => (
+            <div className='block-container w-20 h-20'>
+              <div className='btn-back rounded-xl' />
+              <div className='btn-front rounded-xl flex justify-center items-center'>
+                <img
+                  src={skill.imageUrl}
+                  alt={skill.name}
+                  className='w-1/2 h-1/2 object-contain'
+                />
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </section>
