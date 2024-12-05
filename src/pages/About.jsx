eef944875e426ae860823f120534b67ec2b76313
drealcharlesxavier@proton.me <DRealCharlesXavier@proton.me>
2024-12-04 14:20:07 -1100

    1

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className='max-container'>
+      <h1 className='head-text'>
+        Hello, I'm <span className='blue-gradient_text font-semibold drop-shadow'>Xavier</span>
+      </h1>
+
+      <div>
+        <p className='mt-5 flex flex-col gap-3 text-slate-500'>
+          Software Engineer based in Nigeria, specialising in technical education through hands-on learning and building applications.
+        </p>
+      </div>
+
+      <div className='py-10 flex flex-col'>
+        <h3 className='subhead-text'>My Skills</h3>
+
+        <div className='mt-16 flex flex-wrap gap-12'>
+
+        </div>
+      </div>
+    </section>
   )
 }
 
