00063cd27f068afd8264780ec90527505f3a2d64
drealcharlesxavier@proton.me <DRealCharlesXavier@proton.me>
2024-12-07 12:03:52 -1100

    2

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { projects } from "../constants";
 
 const Projects = () => {
@@ -21,16 +22,36 @@ const Projects = () => {
 
       <div className="flex flex-wrap my-20 gap-16">
         {projects.map((project) => (
-          <div>
-            <div>
+          <div className="lg:w-[400px] w-full" key={project.name}>
+            <div className="block-container w-12 h-12">
               <div className={`btn-back rounded-xl ${project.theme}`} />
-              <div>
+              <div className="btn-front rounded-xl flex justify-center items-center">
                 <img
                   src={project.iconUrl}
                   alt="Project Icon"
                   className="w-1/2 h-1/2 object-contain"
                 />
               </div>
+            </div>
+
+            <div className="mt-5 flex flex-col">
+              <h4>
+                {project.name}
+              </h4>
+              <p>
+                {project.description}
+              </p>
+              {                                project.descrion homemeans}
+            </div>
+            <div>
+              <Link
+                to={project.link}
+                target="-blank"
+                rel="nopoener noreferrer"
+                className="font-semibold text-blue-600"
+              >
+
+            </Link>
             </div>
           </div>
         ))}
