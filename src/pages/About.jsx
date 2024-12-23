576c77ea2eafa81fd2d72c930a56fb6266d2a0e6
drealcharlesxavier@proton.me <DRealCharlesXavier@proton.me>
2024-12-22 14:42:51 -1100

    finishing touches

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -19,8 +19,7 @@ const About = () => {
 
       <div>
         <p className="mt-5 flex flex-col gap-3 text-slate-500">
-          Software Engineer based in Nigeria, specialising in technical
-          education through hands-on learning and building applications.
+          Software Engineer based in Nigeria, specialising in building applications and is dedicated to creating and scaling cutting-edge software solutions that empower businesses and enhance customer experiences.
         </p>
       </div>
 
