af04652a2516c234c65d96694f91de87f18c29fa
Charles Xavier <115213172+DRealCharlesXavier@users.noreply.github.com>
2025-02-11 13:07:09 -1100

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -19,7 +19,7 @@ const Projects = () => {
           I've embarked on numerous projects throughout the years, but these are
           the ones I hold closest to my heart. Many of them are open source, so
           if you come across something that piques your interest, feel free to
-          explore the codebase and your collaboration is highly valued!
+          explore the codebase and contribute your ideas for further enhancements. Your collaboration is highly valued!
         </p>
       </div>
 
